a29281bb7fb30333c368f69d18710ba786fe8b50
kuluders <66642915+kuluders@users.noreply.github.com>
2020-10-25 21:18:33 +0100

    first push - news

--- src/News.java
+++ src/News.java
@@ -1,3 +1,55 @@
+import java.util.*;
+import java.sql.Timestamp;
+
 public class News {
-    
+	private HashMap<Timestamp, NewsPost> newsFeed = new HashMap<Timestamp, NewsPost>();
+	
+	public News(HashMap<Timestamp, NewsPost> newsFeed) {
+		this.newsFeed = newsFeed;
+	}
+	
+	public void addStory(User author, String title, String text, ArrayList<String> tags) {
+		NewsPost newStory = new NewsPost(author, title, text, tags);
+		Date time = new Date();
+		long timestamp = time.getTime(); 
+		Timestamp hashKey = new Timestamp(timestamp);
+		newsFeed.put(hashKey, newStory);
+	}
+	
+	public void markStoryAsRead(Timestamp storyID) {
+		NewsPost justRead = newsFeed.get(storyID);
+		justRead.markAsRead();
+	}
+	
+	public String displayTopTenNews() {
+		Set<Timestamp> keys = newsFeed.keySet();
+		String topFeed = "";
+		int count = 0;
+		Timestamp start = null;
+		ArrayList <Timestamp> top = new ArrayList<Timestamp>();
+		for (Timestamp key:keys) {
+			count++;
+			if (count <= 10)
+				top.add(count-1, key);
+			else {
+				for(int i = 0; i < 5; i++) {
+					if (key.after(top.get(i)) && key.before(top.get(i+5)))
+						start = key;
+					else {
+						if (!key.after(top.get(i)))
+							top.add(i, start);
+						else
+							top.add(i+5, start);
+						top.remove(top.get(top.size()-1));
+					}//else
+				}//for
+			}//else
+		}//for
+		for (Timestamp t:top) {
+			NewsPost post = newsFeed.get(t);
+			topFeed += post.toString() + "\n\n";
+		}
+		return topFeed;
+	}//displayTopTenNews
+	
 }
---
+++ src/NewsPost.java
@@ -0,0 +1,47 @@
+import java.time.*;
+import java.time.format.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class NewsPost {
+	
+	private String content, title, timeOfPosting;
+	private User author;
+	//private ArrayList<String> tags;
+	private int read;
+	private HashSet<String> tags;
+	
+	public NewsPost(User author, String title, String content, HashSet<String> tags){
+		LocalDateTime timeNow = LocalDateTime.now();
+		DateTimeFormatter formattedNow = DateTimeFormatter.ofPattern("dd-MM-yy HH:mm:ss");
+		this.author = author;
+		this.title = title;
+		this.content = content;
+		this.tags = tags;
+		timeOfPosting = timeNow.format(formattedNow);
+	}
+	
+	public void markAsRead(){
+		read++;
+	}
+	
+	public int getReads() {
+		return read;
+	}
+	
+	public HashSet<String> getTags(){
+		return tags;
+	}
+	
+	public String toString() {
+		String s = "";
+		s += title.toUpperCase() + "\n";
+		s += author.toString() + "\n";
+		s += content + "\n";
+		s += timeOfPosting + "\n";		
+		for (String tag:tags) {
+			s += "#" + tag + "; ";
+		}
+		return String.format("|%100d|", s);
+	}
+}
