f8ecf0e5292c06f2afde790c0bea12a927d62d1b
chloelau <cwl119@ic.ac.uk>
2020-10-27 16:51:04 +0000

    (Chloe Lau) Reconstructed User Structure
    Added Files: UserData.java
    
    Updated Files: User.java, UserTest.java

--- src/User.java
+++ src/User.java
@@ -1,150 +1,49 @@
-import java.util.HashMap;
-import java.util.LinkedList;
+import java.util.Date;
 
-public class User {
-  /** hashMap that stores all user data */
-  private final HashMap<Integer, LinkedList<String>> hashMap;
-
-  /** the easiest way to implement a userID, could be improved by using a hashed function */
-  private int counter;
-
-  /** User Class Constructor */
-  public User() {
-    this.hashMap = new HashMap<Integer, LinkedList<String>>();
-    this.counter = 0;
+class User {
+  private Integer userId;
+  private String name;
+  private String email;
+  private String phone;
+  private Date joinDate;
+  
+  public User(Integer userId, String name, String email, String phone) {
+    this.userId = userId;
+    this.name = name;
+    this.email = email;
+    this.phone = phone;
+    this.joinDate = new Date();
   }
-
-  /**
-   * Checks if the database has a specific user with the userID
-   *
-   * @param userID unique UserID used when adding user to hashMap
-   * @return (boolean) whether user exists
-   */
-  public boolean hasUser(Integer userID) {
-    return this.hashMap.containsKey(userID);
+  
+  public Integer getUserId() {
+    return userId;
   }
-
-  /**
-   * Get User Details in a LinkedList<String> Pre: hasUser(userID)
-   *
-   * @param userID unique UserID used when adding user to hashMap
-   * @return (LinkedList < String >) [String name, String emailAddress, String phoneNumber]
-   */
-  public LinkedList<String> getUserDetails(Integer userID) {
-    return this.hashMap.get(userID);
+  
+  public String getName() {
+    return name;
   }
-
-  /**
-   * Get User Name in a String Pre: hasUser(userID)
-   *
-   * @param userID unique UserID used when adding user to hashMap
-   * @return (String) user's name
-   */
-  public String getUserName(Integer userID) {
-    return this.hashMap.get(userID).get(0);
+  
+  public String getEmail() {
+    return email;
   }
-
-  /**
-   * Get User Email Address in a String Pre: hasUser(userID)
-   *
-   * @param userID unique UserID used when adding user to hashMap
-   * @return (String) user's email address
-   */
-  public String getUserEmailAddress(Integer userID) {
-    return this.hashMap.get(userID).get(1);
+  
+  public String getPhone() {
+    return phone;
   }
-
-  /**
-   * Get User Phone Number in a String Pre: hasUser(userID)
-   *
-   * @param userID unique UserID used when adding user to hashMap
-   * @return (String) user's phone number
-   */
-  public String getUserPhoneNumber(Integer userID) {
-    return this.hashMap.get(userID).get(2);
+  
+  public Date getJoinDate() {
+    return joinDate;
   }
-
-  /**
-   * Adds new user profile into the hashMap Pre: !hasUser(userID)
-   *
-   * @param name user's name
-   * @param emailAddress user's email address
-   * @param phoneNumber user's phone number
-   */
-  public void addNewUserProfile(String name, String emailAddress, String phoneNumber) {
-    // Pre: Expected user does not already have a user profile
-    LinkedList<String> value = new LinkedList<>();
-    value.add(name);
-    value.add(emailAddress);
-    value.add(phoneNumber);
-
-    this.hashMap.put(counter++, value);
+  
+  public void setName(String name) {
+    this.name = name;
   }
-
-  /**
-   * Prints User Profile to terminal
-   *
-   * @param userID unique UserID used when adding user to hashMap
-   */
-  public void displayUserProfile(Integer userID) {
-
-    System.out.println(makeString(userID));
+  
+  public void setEmail(String email) {
+    this.email = email;
   }
-
-  /**
-   * Make String for displayUserProfile(userID) to print
-   *
-   * @param userID unique UserID used when adding user to hashMap
-   * @return (String) user details in a string
-   */
-  public String makeString(Integer userID) {
-    LinkedList<String> value = this.hashMap.get(userID);
-    return "UserID: "
-        + userID
-        + "\nName: "
-        + value.get(0)
-        + "\nEmail Address: "
-        + value.get(1)
-        + "\nPhone Number: "
-        + value.get(2);
-  }
-
-  /**
-   * Changes the user's name in the hashMap
-   *
-   * @param userID unique UserID used when adding user to hashMap
-   * @param name new name to be change for the user
-   */
-  public void changeUserName(Integer userID, String name) {
-    LinkedList<String> newValue = this.hashMap.get(userID);
-    newValue.set(0, name);
-
-    this.hashMap.replace(userID, newValue);
-  }
-
-  /**
-   * Changes the user's name in the hashMap
-   *
-   * @param userID unique UserID used when adding user to hashMap
-   * @param newEmailAddress new email address to be change for the user
-   */
-  public void changeUserEmailAddress(Integer userID, String newEmailAddress) {
-    LinkedList<String> newValue = this.hashMap.get(userID);
-    newValue.set(1, newEmailAddress);
-
-    this.hashMap.replace(userID, newValue);
-  }
-
-  /**
-   * Changes the user's name in the hashMap
-   *
-   * @param userID unique UserID used when adding user to hashMap
-   * @param newPhoneNumber new phone number to be change for the user
-   */
-  public void changeUserPhoneNumber(Integer userID, String newPhoneNumber) {
-    LinkedList<String> newValue = this.hashMap.get(userID);
-    newValue.set(2, newPhoneNumber);
-
-    this.hashMap.replace(userID, newValue);
+  
+  public void setPhone(String phone) {
+    this.phone = phone;
   }
 }
--- test/UserTest.java
+++ test/UserTest.java
@@ -1,6 +1,6 @@
 import org.junit.jupiter.api.Test;
 
-import java.util.LinkedList;
+import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -11,71 +11,71 @@ public class UserTest {
   String DEFAULT_PHONE = "01234 567890";
 
   @Test
-  public void addUserTest() {
-    User user = new User();
-    user.addNewUserProfile(DEFAULT_NAME, DEFAULT_EMAIL, DEFAULT_PHONE);
-
-    LinkedList<String> list = new LinkedList<>();
-    list.add(DEFAULT_NAME);
-    list.add(DEFAULT_EMAIL);
-    list.add(DEFAULT_PHONE);
-
-    assertEquals(user.getUserDetails(0), list);
-  }
-
-  @Test
   public void checkUserIDExistTest() {
-    User user = new User();
-    user.addNewUserProfile(DEFAULT_NAME, DEFAULT_EMAIL, DEFAULT_PHONE);
+    UserData userData = new UserData();
+    userData.addNewUserProfile(DEFAULT_NAME, DEFAULT_EMAIL, DEFAULT_PHONE);
 
-    assertTrue(user.hasUser(0));
+    assertTrue(userData.hasUser(0));
   }
-
+  
+  @Test
+  public void addUserTest() {
+    UserData userData = new UserData();
+    int userId = userData.addNewUserProfile(DEFAULT_NAME, DEFAULT_EMAIL, DEFAULT_PHONE);
+    User user = userData.getUser(userId);
+    assertEquals(user.getName(), DEFAULT_NAME);
+    assertEquals(user.getEmail(), DEFAULT_EMAIL);
+    assertEquals(user.getPhone(), DEFAULT_PHONE);
+    assertEquals(user.getUserId(), userId);
+  }
+  
   @Test
   public void changeNameTest() {
-    User user = new User();
-    user.addNewUserProfile(DEFAULT_NAME, DEFAULT_EMAIL, DEFAULT_PHONE);
+    UserData userData = new UserData();
+    int userId = userData.addNewUserProfile(DEFAULT_NAME, DEFAULT_EMAIL, DEFAULT_PHONE);
     String NEW_NAME = "Newname Surname";
-    user.changeUserName(0, NEW_NAME);
+    userData.changeUserName(userId, NEW_NAME);
 
-    assertEquals(user.getUserName(0), NEW_NAME);
+    assertEquals(userData.getUserName(userId), NEW_NAME);
   }
 
   @Test
   public void changeEmailTest() {
-    User user = new User();
-    user.addNewUserProfile(DEFAULT_NAME, DEFAULT_EMAIL, DEFAULT_PHONE);
+    UserData userData = new UserData();
+    int userId = userData.addNewUserProfile(DEFAULT_NAME, DEFAULT_EMAIL, DEFAULT_PHONE);
     String NEW_EMAIL = "newuser@domain.com";
-    user.changeUserEmailAddress(0, NEW_EMAIL);
+    userData.changeUserEmailAddress(userId, NEW_EMAIL);
 
-    assertEquals(user.getUserEmailAddress(0), NEW_EMAIL);
+    assertEquals(userData.getUserEmailAddress(userId), NEW_EMAIL);
   }
 
   @Test
   public void changePhoneNumTest() {
-    User user = new User();
-    user.addNewUserProfile(DEFAULT_NAME, DEFAULT_EMAIL, DEFAULT_PHONE);
+    UserData userData = new UserData();
+    int userId = userData.addNewUserProfile(DEFAULT_NAME, DEFAULT_EMAIL, DEFAULT_PHONE);
     String NEW_PHONE = "00000 000000";
-    user.changeUserPhoneNumber(0, NEW_PHONE);
+    userData.changeUserPhoneNumber(userId, NEW_PHONE);
 
-    assertEquals(user.getUserPhoneNumber(0), NEW_PHONE);
+    assertEquals(userData.getUserPhoneNumber(userId), NEW_PHONE);
   }
 
   @Test
   public void displayUserProfileTest() {
-    User user = new User();
-    user.addNewUserProfile(DEFAULT_NAME, DEFAULT_EMAIL, DEFAULT_PHONE);
-
+    UserData userData = new UserData();
+    int userId = userData.addNewUserProfile(DEFAULT_NAME, DEFAULT_EMAIL, DEFAULT_PHONE);
+    Date joinDate = userData.getUserJoinDate(userId);
     String display =
         "UserID: "
-            + 0
+            + userId
             + "\nName: "
             + DEFAULT_NAME
             + "\nEmail Address: "
             + DEFAULT_EMAIL
             + "\nPhone Number: "
-            + DEFAULT_PHONE;
-
-    assertEquals(display, user.makeString(0));
+            + DEFAULT_PHONE
+            + "\nJoined at: "
+            + joinDate;
+    
+    assertEquals(display, userData.makeString(userId));
   }
 }
